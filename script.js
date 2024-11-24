8e534fcdcd2c8a27d8f8078f2d3292c60c3749cd
Andria Ghurtchumelia <andriaghurchumelia4@gmail.com>
2024-11-24 19:23:59 +0400

    js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const mainList = document.getElementById("mainList");
 
 let today = new Date();
 let month = today.toLocaleString("default", { month: "long" });
+let listArray = [];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -19,49 +20,94 @@ form.addEventListener("submit", (e) => {
   input.value = "";
 });
 
-function createList (){
-    let div = document.createElement("div");
-    div.classList.add("todo-list-main");
-    mainList.appendChild(div);
-    
-    
-    let todoListDiv = document.createElement("div");
-    todoListDiv.classList.add("todo-list-note");
-    div.appendChild(todoListDiv);
-    
-    let todoListText = document.createElement("div");
-    todoListText.classList.add("todo-list-note-text");
-    todoListDiv.appendChild(todoListText);
-    
-    let h1 = document.createElement("h1");
-    h1.textContent = input.value;
-    todoListText.appendChild(h1);
-    
-    let p = document.createElement("p");
-    p.textContent = `${today.getDate()} ${month} `;
-    todoListText.appendChild(p);
-    
-    let textMark = document.createElement("div");
-    textMark.classList.add("text-mark");
-    todoListDiv.appendChild(textMark);
-    
-    let checkbox = document.createElement("input")
-    checkbox.setAttribute("type" , "checkbox")
-    textMark.appendChild(checkbox)
-    
-    
-    
-    let img = document.createElement("img");
-    img.src = "./images/trash.png";
-    img.addEventListener("click", () => {
-        if(checkbox.checked){
-            div.remove();
-        }
-    });
+function createList() {
+  let div = document.createElement("div");
+  div.classList.add("todo-list-main");
+  mainList.appendChild(div);
 
-    textMark.appendChild(img);
-    
-    localStorage.setItem("item" , input.value)
-    localStorage.getItem("item")
+  let todoListDiv = document.createElement("div");
+  todoListDiv.classList.add("todo-list-note");
+  div.appendChild(todoListDiv);
+
+  let todoListText = document.createElement("div");
+  todoListText.classList.add("todo-list-note-text");
+  todoListDiv.appendChild(todoListText);
+
+  let h1 = document.createElement("h1");
+  h1.textContent = input.value;
+  todoListText.appendChild(h1);
+
+  let p = document.createElement("p");
+  p.textContent = `${today.getDate()} ${month} `;
+  todoListText.appendChild(p);
+
+  let textMark = document.createElement("div");
+  textMark.classList.add("text-mark");
+  todoListDiv.appendChild(textMark);
+
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  textMark.appendChild(checkbox);
+
+  let img = document.createElement("img");
+  img.src = "./images/trash.png";
+  img.addEventListener("click", () => {
+    if (checkbox.checked) {
+      div.remove();
+      getItem.filter((param) =>{
+        return param !== param
+      })
+    }
+  });
+
+  textMark.appendChild(img);
+  listArray.push(input.value);
+  localStorage.setItem("item", JSON.stringify(listArray));
 }
+let getItem = JSON.parse( localStorage.getItem("item"));
 
+getItem.map((value) =>{
+
+  
+  let div = document.createElement("div");
+  div.classList.add("todo-list-main");
+  mainList.appendChild(div);
+
+  let todoListDiv = document.createElement("div");
+  todoListDiv.classList.add("todo-list-note");
+  div.appendChild(todoListDiv);
+
+  let todoListText = document.createElement("div");
+  todoListText.classList.add("todo-list-note-text");
+  todoListDiv.appendChild(todoListText);
+
+  let h1 = document.createElement("h1");
+  h1.textContent = value;
+  todoListText.appendChild(h1);
+
+  let p = document.createElement("p");
+  p.textContent = `${today.getDate()} ${month} `;
+  todoListText.appendChild(p);
+
+  let textMark = document.createElement("div");
+  textMark.classList.add("text-mark");
+  todoListDiv.appendChild(textMark);
+
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  textMark.appendChild(checkbox);
+
+  let img = document.createElement("img");
+  img.src = "./images/trash.png";
+  img.addEventListener("click", () => {
+    if (checkbox.checked) {
+      div.remove();
+      getItem.filter((param) =>{
+        return param !== param
+      })
+    }
+  });
+
+  textMark.appendChild(img);
+})
+console.log(getItem);
