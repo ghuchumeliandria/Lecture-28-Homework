741c37eb8f91cf48f74a5ecadbc0a9a3d9b0b515
Andria Ghurtchumelia <andriaghurchumelia4@gmail.com>
2024-11-26 19:33:15 +0400

    dzliiivs

--- script.js
+++ script.js
@@ -5,22 +5,52 @@ const todoList = document.getElementById("todoList");
 const submitBtn = document.getElementById("submitBtn");
 const trash = document.getElementById("trash");
 const mainList = document.getElementById("mainList");
+const dayTime = document.getElementById("dayTime");
+const day = document.getElementById("day")
 
-let today = new Date();
-let month = today.toLocaleString("default", { month: "long" });
-let listArray = [];
+const today = new Date();
+const dayNum = today.getDate()
+const days = today.getDay();
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const daysFn = (param) => weekDays[param];
 
+// console.log(daysFn(days) ,dayNum );
+let data = JSON.parse(localStorage.getItem("task")) || [];
+
+let dateTime = new Date();
+let hours = dateTime.getHours();
+let minutes = dateTime.getMinutes();
+dayTime.textContent = `${hours}:${minutes}`;
+day.textContent = ` ${daysFn(days)} ${dayNum}`
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value == "") {
-    alert("");
+    alert("გთხოვ შეიყვანე ტექსტი");
+  } else if (input.value.length > 10) {
+    alert("შეიყვანე მოკლე შეტყობინება");
   } else {
-    createList();
+    let taskOb = {
+      id: input.value,
+      time: Date().slice(0, Date().length - 33),
+      check: false,
+    };
+    createList(taskOb);
+    data.push(taskOb);
+    localStorage.setItem("task", JSON.stringify(data));
   }
+
   input.value = "";
 });
 
-function createList() {
+function createList(task) {
   let div = document.createElement("div");
   div.classList.add("todo-list-main");
   mainList.appendChild(div);
@@ -34,11 +64,11 @@ function createList() {
   todoListDiv.appendChild(todoListText);
 
   let h1 = document.createElement("h1");
-  h1.textContent = input.value;
+  h1.textContent = task.id;
   todoListText.appendChild(h1);
 
   let p = document.createElement("p");
-  p.textContent = `${today.getDate()} ${month} `;
+  p.textContent = task.time;
   todoListText.appendChild(p);
 
   let textMark = document.createElement("div");
@@ -47,67 +77,44 @@ function createList() {
 
   let checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox");
+  checkbox.classList.add("radio-btn");
+  checkbox.checked = task.check;
   textMark.appendChild(checkbox);
+
+  checkbox.addEventListener("click", () => {
+    task.check = checkbox.checked;
+    if (task.check === true) {
+      h1.classList.add("list-line");
+      p.classList.add("list-line");
+    } else {
+      h1.classList.remove("list-line");
+      p.classList.remove("list-line");
+    }
+    localStorage.setItem("task", JSON.stringify(data));
+  });
+
+  if (task.check === true) {
+    h1.classList.add("list-line");
+    p.classList.add("list-line");
+  } else {
+    h1.classList.remove("list-line");
+    p.classList.remove("list-line");
+  }
 
   let img = document.createElement("img");
   img.src = "./images/trash.png";
+
   img.addEventListener("click", () => {
-    if (checkbox.checked) {
-      div.remove();
-      getItem.filter((param) =>{
-        return param !== param
-      })
-    }
+    div.remove();
+    data = data.filter((param) => {
+      return param.id != task.id;
+    });
+    localStorage.setItem("task", JSON.stringify(data));
   });
 
   textMark.appendChild(img);
-  listArray.push(input.value);
-  localStorage.setItem("item", JSON.stringify(listArray));
 }
-let getItem = JSON.parse( localStorage.getItem("item"));
 
-getItem.map((value) =>{
-
-  
-  let div = document.createElement("div");
-  div.classList.add("todo-list-main");
-  mainList.appendChild(div);
-
-  let todoListDiv = document.createElement("div");
-  todoListDiv.classList.add("todo-list-note");
-  div.appendChild(todoListDiv);
-
-  let todoListText = document.createElement("div");
-  todoListText.classList.add("todo-list-note-text");
-  todoListDiv.appendChild(todoListText);
-
-  let h1 = document.createElement("h1");
-  h1.textContent = value;
-  todoListText.appendChild(h1);
-
-  let p = document.createElement("p");
-  p.textContent = `${today.getDate()} ${month} `;
-  todoListText.appendChild(p);
-
-  let textMark = document.createElement("div");
-  textMark.classList.add("text-mark");
-  todoListDiv.appendChild(textMark);
-
-  let checkbox = document.createElement("input");
-  checkbox.setAttribute("type", "checkbox");
-  textMark.appendChild(checkbox);
-
-  let img = document.createElement("img");
-  img.src = "./images/trash.png";
-  img.addEventListener("click", () => {
-    if (checkbox.checked) {
-      div.remove();
-      getItem.filter((param) =>{
-        return param !== param
-      })
-    }
-  });
-
-  textMark.appendChild(img);
-})
-console.log(getItem);
+data.forEach((value) => {
+  createList(value);
+});
